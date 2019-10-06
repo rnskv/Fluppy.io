9ccf1906956097a6063e867c5893a36babe04b4b
Roman Nuyskov <web.rnskv@gmail.com>
2019-10-06 22:10:24 +0300

    Add eyes

--- client/src/entities/Player.js
+++ client/src/entities/Player.js
@@ -17,9 +17,18 @@ class Player extends GameObject {
         const container = new PIXI.Container();
         const graphics = new PIXI.Graphics();
 
+        graphics.beginFill(0x00FF00, 1);
+        graphics.lineStyle(3, 0x000000);
+        graphics.drawCircle(0, 0, this.radius);
+
+        graphics.beginFill(0x000000, 1);
+        graphics.lineStyle(1, 0x000000);
+        graphics.drawCircle(this.radius * 0.5, this.radius * 0.05, 5);
+
         graphics.beginFill(0xFFFFFF, 1);
         graphics.lineStyle(1, 0x000000);
-        graphics.drawCircle(0, 0, this.radius);
+        graphics.drawCircle(this.radius * 0.5, this.radius * 0.05, 3);
+
 
         // if (this.isCurrentPlayer && this.x < 500) {
         //     graphics.beginFill(0x000000, 0.0);
@@ -38,7 +47,7 @@ class Player extends GameObject {
 
     createStaticObject() {
         console.log(this)
-        const text = new PIXI.Text(`${this.id.toString().slice(0, 5)}`,{fontFamily : 'Arial', fontSize: 14, fill : 0x0000, align : 'center'});
+        const text = new PIXI.Text(`${this.id.toString().slice(0, 5)}`,{fontFamily : 'Roboto', fontSize: 16, fill : 0xffffff, align : 'center', stroke: 0x000000, strokeThickness: 3});
         text.position.y = this.radius + 10;
         text.position.x = 0;
         text.anchor = new PIXI.Point(0.5, 0);
