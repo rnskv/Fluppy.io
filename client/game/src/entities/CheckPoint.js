b4bbc8749e9de98c6505fb1982a56d0eadb29016
rnskv <web.rnskv@gmail.com>
2019-10-23 14:05:57 +0300

    Rename offsets to padding

--- client/game/src/entities/CheckPoint.js
+++ client/game/src/entities/CheckPoint.js
@@ -4,14 +4,14 @@ import * as PIXI from "pixi.js";
 class CheckPoint extends GameObject {
   constructor({...params}) {
     super({...params})
-    this.offsets = 40;
+    this.padding = 40;
   }
 
   createObject() {
     const container = new PIXI.Container();
     const graphics = new PIXI.Graphics();
     graphics.beginFill(0x00ff00);
-    graphics.drawRect(0, this.offsets / 2, this.width, this.height - this.offsets);
+    graphics.drawRect(0, this.padding / 2, this.width, this.height - this.padding);
 
     container.addChild(graphics);
 
